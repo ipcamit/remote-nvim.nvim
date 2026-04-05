aa9e944ea016290061784cf3b3e0d0fc10f66a30
Amit Gupta <amit_@outlook.in>
2026-04-05 19:28:49 +0200

    debug: add canary file + move vim.notify into vim.schedule

--- lua/remote-nvim/providers/provider.lua
+++ lua/remote-nvim/providers/provider.lua
@@ -823,12 +823,16 @@ function Provider:_wait_for_server_to_be_ready()
   end
 
   -- Start the timer
-  timer:start(timeout, 0, vim.schedule_wrap(function()
-    vim.notify(("Server did not come up on local in %s ms. Try again :("):format(timeout), vim.log.levels.ERROR)
+  timer:start(timeout, 0, function()
     timer:stop()
     timer:close()
-    error(("Server did not come up on local in %s ms. Try again :("):format(timeout))
-  end))
+    -- Canary: touch a file so we know this exact code path was hit
+    local f = io.open("/tmp/remote_nvim_timer_fired", "w")
+    if f then f:write(tostring(os.time())); f:close() end
+    vim.schedule(function()
+      vim.notify(("Server did not come up on local in %s ms. Try again :("):format(timeout), vim.log.levels.ERROR)
+    end)
+  end)
   probe_server_readiness()
 end
 
